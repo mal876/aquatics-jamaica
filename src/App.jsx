6d139446cd674c7aa700249bf4c08cbc31c0d012
Malik Dekid <maldekid876@gmail.com>
2025-02-13 13:09:55 -0500

    Back on track
    update

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar"
+
+export default function App() {
+  
+  return (
+      <div>
+        <h1 className="text-5xl font-bold underline text-center">
+          Hello world!
+        </h1>
+        <Navbar></Navbar>
+      </div>
+  )
+}
+
+
